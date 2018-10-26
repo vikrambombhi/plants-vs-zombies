8c136ab7c17096a689fc922e33c1c5d57914812d
vikrambombhi <vbombhi@gmail.com>
2018-10-25 20:10:30 -0400

    base sunflower

--- src/Piece.java
+++ src/Piece.java
@@ -1,8 +1,11 @@
 public abstract class Piece {
   private char type;
 
+  // This is not a good way to keep track of types
+  // maybe use a array, enum, or hashmap as needed in the future
   protected char normalZombie = 'Z';
   protected char sunFlower = 'S';
+  protected char peaShooter = 'P';
 
   protected char getType() {
     return this.type;
@@ -12,7 +15,4 @@ public abstract class Piece {
     this.type = type;
   }
 
-  protected void setRandomZombieType() {
-    this.setType(normalZombie);
-  }
 }
