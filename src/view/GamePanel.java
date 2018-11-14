67dfa0fc22e5e0c40a30c8763861ed86cfd2d883
vikrambombhi <vbombhi@gmail.com>
2018-11-14 14:24:55 -0500

    get board size from Board class

--- src/view/GamePanel.java
+++ src/view/GamePanel.java
@@ -6,19 +6,19 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import model.Board;
+
 public class GamePanel {
 
-	/* Set up for the game's field of play */
-	private static final int ROWS = 5;
-	private static final int COLS = 10;
-	
 	GridLayout gameField;
 	JButton[][] gameFieldSlot;
 	JPanel gamePanel;
 	
 	public GamePanel() {
+        int ROWS = Board.getHeight();
+        int COLS = Board.getHeight();
 		gameFieldSlot = new JButton[ROWS][COLS];
-		gameField = new GridLayout(ROWS,COLS);
+		gameField = new GridLayout(ROWS, COLS);
 		gamePanel = new JPanel();
 		gamePanel.setPreferredSize(new Dimension(450,250));
 		gamePanel.setLayout(gameField);
@@ -26,7 +26,6 @@ public class GamePanel {
 		for(int x = 0; x < ROWS; x++) {
 			for (int y = 0; y < COLS; y++) {
 				gameFieldSlot[x][y] = new JButton();
-//				gameFieldSlot[x][y].setEnabled(false);
 				gamePanel.add(gameFieldSlot[x][y]);
 			}
 		}
