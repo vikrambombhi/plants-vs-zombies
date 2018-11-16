88b4f89c528f87b718e5e08b1ffb2ffeeadcb878
vikrambombhi <vbombhi@gmail.com>
2018-11-16 12:29:15 -0500

    update entire board gui on board event

--- src/event/BoardEvent.java
+++ src/event/BoardEvent.java
@@ -2,29 +2,20 @@ package event;
 
 import java.util.EventObject;
 
+import model.Tile;
+
 @SuppressWarnings("serial")
 public class BoardEvent extends EventObject {
 
-    private int row;
-    private int col;
-    private char type;
+    private Tile[][] tiles;
 
-    public BoardEvent(Object source, int row, int col, char type) {
+    public BoardEvent(Object source, Tile[][] tiles) {
         super(source);
-        this.row = row;
-        this.col = col;
-        this.type = type;
+        this.tiles = tiles;
     }
 
-    public int getRow() {
-        return this.row;
-    }
 
-    public int getCol() {
-        return this.col;
-    }
-
-    public char getType() {
-        return this.type;
+    public Tile[][] getTiles() {
+        return this.tiles;
     }
 }
