47e0c087fd7342408cec3fad7da01b157aa99cf4
vikrambombhi <vbombhi@gmail.com>
2018-11-16 12:27:14 -0500

    make plant select radio buttons

--- src/view/PlantSelectionPanel.java
+++ src/view/PlantSelectionPanel.java
@@ -3,7 +3,7 @@ package view;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
-import javax.swing.JButton;
+import javax.swing.JRadioButton;
 import javax.swing.JPanel;
 
 /* This is a Subclass that builds the MainView class.
@@ -11,17 +11,17 @@ import javax.swing.JPanel;
  *  buy to place on the game field. */ 
 public class PlantSelectionPanel {
 	private static final int MAX_PLANTS = 5;
-	private JButton[] plants;
+	private JRadioButton[] plants;
 	private JPanel shopPanel;
 
 	
 	public PlantSelectionPanel() {
-		plants = new JButton[MAX_PLANTS];
+		plants = new JRadioButton[MAX_PLANTS];
 		shopPanel = new JPanel();
 		shopPanel.setPreferredSize(new Dimension(200,40));
 		
 		for ( int i = 0; i < MAX_PLANTS; i++) {
-			plants[i] = new JButton("Plant: " + (i+1)); //fix this
+			plants[i] = new JRadioButton("Plant: " + (i+1)); //fix this
 			shopPanel.add(plants[i]);
 		}
 	}
