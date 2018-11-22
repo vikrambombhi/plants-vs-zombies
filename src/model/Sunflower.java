eb0ee8d8a439708a9f3b509e80d8bfd6a5233be0
vikrambombhi <vbombhi@gmail.com>
2018-11-21 20:47:02 -0500

    increase sun points generated stats when sunflower is created

--- src/model/Sunflower.java
+++ src/model/Sunflower.java
@@ -1,12 +1,18 @@
 package model;
+
+import model.Stats;
+
 /* Sunflower Class is an essential entity for the game.
  * Sunflower generates cashflow for the player to buy new plants.*/
 public class Sunflower extends Plants {
 
 	private final int SUN_POINTS_GENERATED = 3;
+    private Stats stats;
 
 	public Sunflower() {
 		super(10, 8, 2);
+        this.stats = Stats.getStats();
+        this.stats.increaseSunPointsGenerationRate(SUN_POINTS_GENERATED);
 	}
 
 	/*
