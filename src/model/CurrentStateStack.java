e662c82667918698db5cd419ed2c9ebc191783d3
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2018-11-25 17:50:50 -0500

    Saves currentState onto stack.

--- src/model/CurrentStateStack.java
+++ src/model/CurrentStateStack.java
@@ -5,7 +5,12 @@ import java.util.Stack;
 public class CurrentStateStack {
 //	private static Stack<LevelInfo> pastLevel;
 //	private static Stack<LevelInfo> futureLevel;
-	private static Stack<PlayerInfo> pastPlayer;
-	private static Stack<PlayerInfo> futurePlayer;
+	private static Stack<PlayerInfo> pastMove;
+	private static Stack<PlayerInfo> futureMove;
+	
+	public void saveCurrentState(PlayerInfo currentPlayerInfo) throws CloneNotSupportedException {
+		pastMove.push((PlayerInfo) currentPlayerInfo.clone());
+		futureMove.clear();
+	}
 
 }
