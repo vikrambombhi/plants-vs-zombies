7cafbea500ecb396098c7302d4e701245369adb8
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2018-11-25 17:45:58 -0500

    StateStack for push and pop Player and Level Data/Info

---
+++ src/model/CurrentStateStack.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.Stack;
+
+public class CurrentStateStack {
+//	private static Stack<LevelInfo> pastLevel;
+//	private static Stack<LevelInfo> futureLevel;
+	private static Stack<PlayerInfo> pastPlayer;
+	private static Stack<PlayerInfo> futurePlayer;
+
+}
