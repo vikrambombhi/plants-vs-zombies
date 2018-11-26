6efe698bfc7aa3c7e860e63b9a6aae213e01b564
vikrambombhi <vbombhi@gmail.com>
2018-11-25 20:39:35 -0500

    documentation for controllers

--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -14,6 +14,9 @@ import model.Sunflower;
 import view.PlantSelectionPanel;
 import view.StatsPanel;
 
+/*
+ * Controller for the game board
+ */
 public class BoardController implements ActionListener {
     private int row;
     private int col;
@@ -21,6 +24,7 @@ public class BoardController implements ActionListener {
     private PlantSelectionPanel plantSelectionPanel;
     private StatsPanel statsPanel;
     
+    // Create controller
     public BoardController(int row, int col, PlantSelectionPanel plantSelectionPanel, StatsPanel statsPanel) {
         this.row = row;
         this.col = col;
@@ -29,19 +33,33 @@ public class BoardController implements ActionListener {
         this.statsPanel = statsPanel;
     }
 
+    // Handle action when any tile on the board is clicked
+    // Currently this is how users place plants on the board.
 	public void actionPerformed(ActionEvent e) {
+        // Get plant selected from plant selection view
         String plantType = plantSelectionPanel.getPlant();
 
+        // Check if selected plant is of type sunflower
         if (plantType == PlantTypes.SUNFLOWER.getName()) {
+            // Create sunflower
             Sunflower sunflower = new Sunflower();
+            // Check if user has enough sunpoints to buy a sunflower
             if (sunflower.getSunPointCost() <= this.statsPanel.getStats().getSunPoints()) {
+                // try to place sunflower on board if there is 
+                // this can fail if there is already a plant at selected location for example
                 if (this.board.placePlant(sunflower, this.row, this.col) == true) {
+                    // remove sunpoints required to buy the sunflower from the user
                     this.statsPanel.getStats().increaseSunPointsGenerationRate(sunflower.getSunPointGeneratedPerTurn());
                 }
             }
+            // Check if selected plant is of type peashooter
         } else if (plantType == PlantTypes.PEASHOOTER.getName()) {
+            // Create peashooter
             Peashooter peashooter = new Peashooter();
+            // Check if user has enough sunpoints to buy a peashooter
             if (peashooter.getSunPointCost() <= this.statsPanel.getStats().getSunPoints()) {
+                // Place peashooter on board
+                // TODO: remove sunpoints from user
                 this.board.placePlant(peashooter, this.row, this.col);
             }
         }
--- src/controller/NextTurnController.java
+++ src/controller/NextTurnController.java
@@ -8,18 +8,23 @@ import model.Board;
 import model.Stats;
 import view.StatsPanel;
 
+/*
+ * This controller handles when the user wants to go to the next turn, redo, or undo 
+ */
 public class NextTurnController implements ActionListener {
 
     private Board board;
     private StatsPanel statsPanel;
     private Stats stats;
 
+    // Create controller
     public NextTurnController(StatsPanel panel) {
         this.board = Board.getBoard();
         this.statsPanel = panel;
         this.stats = Stats.getStats();
     }
 
+    // Handle what the user wants to do, for now only the ability to goto the next turn is supported
     public void actionPerformed(ActionEvent e) {
         this.board.turn();
         this.stats.incrementTurn();
