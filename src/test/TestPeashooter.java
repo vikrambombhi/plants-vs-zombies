d940589001146f63789417c30fe946844cb1b443
vikrambombhi <vbombhi@gmail.com>
2018-11-25 23:47:09 -0500

    covered all plant functions for peashooter, offensive only functions still need to be tested

--- src/test/TestPeashooter.java
+++ src/test/TestPeashooter.java
@@ -28,6 +28,36 @@ public class TestPeashooter {
     public void testPeashooterToString() {
         assertEquals("Peashooter name should be 'Peashooter'", "Peashooter", peashooter.toString());
     }
+
+    @Test
+    public void tesPeashooterHp() {
+        assertEquals("Peashooter HP should be 10", 10, peashooter.getHP());
+    }
+
+    @Test
+    public void testPeashooterTakeDamage() {
+        peashooter.takeDamage(1);
+        assertEquals("Peashooter HP should be 9", 9, peashooter.getHP());
+    }
+
+    @Test
+    public void testPeashooterTakeExtraDamage() {
+        // Way more hp the peashooter has
+        peashooter.takeDamage(10000);
+        // Peashooter HP should never be negitive only 0
+        assertEquals("Peashooter HP should be 0", 0, peashooter.getHP());
+    }
+
+    @Test
+    public void testPeashooterCost() {
+        assertEquals("Peashooter cost should be 15", 15, peashooter.getSunPointCost());
+    }
+
+    @Test
+    public void testPeashooterAbilityFrequency() {
+        assertEquals("Peashooter ability frequency should be 2", 2, peashooter.getAbilityFrequency());
+    }
+
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(TestPeashooter.class);
         for (Failure failure : result.getFailures()) {
