ed1457d2ea4227871ff16c78b20db54385eb7f1d
vikrambombhi <vbombhi@gmail.com>
2018-12-05 00:12:15 -0500

    save config in xml file

--- src/controller/SettingsController.java
+++ src/controller/SettingsController.java
@@ -12,11 +12,21 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 import view.MainView;
 
 public class SettingsController implements ActionListener {
 	private MainView gameInterface;
-	
+
 	public SettingsController(MainView gameInterface) {
         this.gameInterface = gameInterface;
 		gameInterface.addActionListenerSettingsController(this);
@@ -48,21 +58,56 @@ public class SettingsController implements ActionListener {
         }
     }
 
-	
+    public void saveConfig(File outputFile, int configHeight, int configWidth) {
+        try {
+         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+         Document doc = dBuilder.newDocument();
+
+         // root element
+         Element rootElement = doc.createElement("plants-vs-zombies");
+         doc.appendChild(rootElement);
+
+         // supercars element
+         Element board = doc.createElement("board");
+         rootElement.appendChild(board);
+
+         // carname element
+         Element height = doc.createElement("height");
+         height.appendChild(doc.createTextNode(Integer.toString(configHeight)));
+         board.appendChild(height);
+
+         // carname element
+         Element width = doc.createElement("width");
+         width.appendChild(doc.createTextNode(Integer.toString(configWidth)));
+         board.appendChild(width);
+
+         // write the content into xml file
+         TransformerFactory transformerFactory = TransformerFactory.newInstance();
+         Transformer transformer = transformerFactory.newTransformer();
+         DOMSource source = new DOMSource(doc);
+         StreamResult result = new StreamResult(outputFile);
+         transformer.transform(source, result);
+
+         // Output to console for testing
+         StreamResult consoleResult = new StreamResult(System.out);
+         transformer.transform(source, consoleResult);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+    }
+
 	public void actionPerformed(ActionEvent e) {
-		
-		/* 
+		/*
          * This action creates the game by making the game visible.
          * This also enables and disables some of the menu selections.
          */
 		if(e.getSource() == gameInterface.getCreatePVZGame()) {
-			
 			gameInterface.getCreatePVZGame().setEnabled(false);
 			gameInterface.getSavePVZGame().setEnabled(true);
 			gameInterface.getCreateMap().setEnabled(true);
 			gameInterface.getLoadMap().setEnabled(true);
 			gameInterface.getContentPane().setVisible(true);
-			
 			System.out.println(e.getActionCommand());
 		}
 
@@ -76,8 +121,9 @@ public class SettingsController implements ActionListener {
             int returnVal = fc.showSaveDialog(gameInterface);
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 File file = fc.getSelectedFile();
-                // TODO: create xml here.
-                System.out.println("Saving: " + file.getName());
+                System.out.println("Saving file " + file.getName());
+                // TODO: ask user for height and width
+                saveConfig(file, 10, 50);
             } else {
                 System.out.println("Open command cancelled by user.");
             }
