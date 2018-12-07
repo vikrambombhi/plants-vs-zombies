f12cdb6e650b6c1b945d0d8f3d0c6dc0610a2f87
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2018-12-07 16:39:46 -0500

    Undo/Redo button now becomes unselectable based on the logic of the game

--- src/controller/UndoTurnController.java
+++ src/controller/UndoTurnController.java
@@ -2,6 +2,9 @@ package controller;
 
 import java.awt.event.ActionListener;
 import java.io.IOException;
+
+import javax.swing.JButton;
+
 import java.awt.event.ActionEvent;
 
 import event.StatsEvent;
@@ -18,10 +21,12 @@ public class UndoTurnController implements ActionListener {
     private Stats stats;
     private TurnStackBoard turnStackBoard;
     private TurnStackStats turnStackStats;
+    private JButton[] decisions;
 
-    public UndoTurnController(StatsPanel panel) {
+    public UndoTurnController(StatsPanel panel, JButton[] decisions) {
         this.board = Board.getBoard();
         this.statsPanel = panel;
+        this.decisions = decisions;
         this.stats = Stats.getStats();
         this.turnStackBoard = TurnStackBoard.getTurnStackBoard();
         this.turnStackStats = TurnStackStats.getTurnStackStats();
@@ -31,6 +36,8 @@ public class UndoTurnController implements ActionListener {
         try {
             this.board.undoBoard(this.turnStackBoard.undoTurn());
             this.stats.undoStats(this.turnStackStats.undoTurn());
+            
+            decisions[2].setEnabled(true);
         } catch (IOException ex) {
             ex.printStackTrace();
         } catch (ClassNotFoundException ex) {
